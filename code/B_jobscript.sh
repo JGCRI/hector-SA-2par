a8358294ba954fed0480db7a7ee8f3d26f9b991e
Leyang Feng <leyang.feng@pnnl.gov>
2017-09-20 12:46:57 -0400

    Job script for running B*.R script on PIC.

---
+++ code/B_jobscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -A GCAM
+#SBATCH -t 10:00:00
+#SBATCH -N 1
+#SBATCH -p shared
+#SBATCH -n 1
+
+#Set up your environment you wish to run in with module commands.
+module purge
+module load R/3.2.0
+module load gcc/5.2.0
+
+#Actually codes starts here
+now=$(date)
+echo "Current time : $now"
+
+export BOOSTROOT=/pic/projects/GCAM/GCAM-libraries/include
+export BOOSTLIB=/pic/projects/GCAM/GCAM-libraries/lib/boost
+
+Rscript /people/feng999/CMS/hector-SA-2par/code/B.ini_generation_and_pic_run.R --nosave --no-restore
+
+now=$(date)
+echo "Current time : $now"
