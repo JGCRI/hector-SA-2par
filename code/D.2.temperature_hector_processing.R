5fe5a446422f44ab192c58cab201d457c442f9e5
Leyang Feng <leyang.feng@pnnl.gov>
2017-09-21 12:40:20 -0400

    Processing of hector Tgav -- referenced by 1951: 1990 then applied 15 years moving average.

---
+++ code/D.2.temperature_hector_processing.R
@@ -0,0 +1,37 @@
+library( 'tidyr' )
+library( 'readxl' )
+library( 'smooth' )
+library( 'Mcomp' )
+setwd( 'c:/Users/feng999/Documents/CMS/hector-SA-2par' )
+
+# ---
+# 1. read in processed observation
+observation <- read.csv( './int-out/D.temperature_obervation_ma.csv', stringsAsFactors = F )
+observation_years <- sort( unique( observation$year ) ) 
+
+# --- 
+# 2. read in hector Tgav and calculate standarized Tgav
+hector_res <- read.csv( './int-out/C.hector_run_cleanup.csv', stringsAsFactors = F )
+hector_tgav <- hector_res[ hector_res$variable == 'Tgav', ]
+tgav_crop <- hector_tgav[ , c( 'run_name', 'variable', 'units', paste0( 'X', observation_years ) ) ]
+tgav_crop$ref_mean <- apply( tgav_crop[ , c( paste0( 'X', 1951 : 1990 ) ) ], 1, mean )
+tgav_crop[ , c( paste0( 'X', observation_years ) ) ] <- tgav_crop[ , c( paste0( 'X', observation_years ) ) ] -  tgav_crop$ref_mean
+tgav_crop$ref_mean <- NULL
+
+# ---
+# 3. experiment on tgav_crop -- moving average for each hector run 
+tgav_ma_res_list <- lapply( 1 : nrow( tgav_crop ), function( i ) { 
+  #print( i )
+  temp_line <- tgav_crop[ i, ]
+  run_name <- temp_line$run_name 
+  run_ts <- unlist( temp_line[ , paste0( 'X', ob_year_list ) ] )
+  run_ts_ma <- mav( run_ts, 15 ) 
+  out_df <- data.frame( t( c( run_name, run_ts_ma ) ) )
+  colnames( out_df ) <- c( 'run_name', paste0( 'X', observation_years ) )
+  return( out_df )
+} )
+tgav_crop_ma <- do.call( 'rbind', tgav_ma_res_list )
+
+write.csv( tgav_crop_ma, './int-out/D.temperature_hector_ma.csv', row.names = F )
+
+
