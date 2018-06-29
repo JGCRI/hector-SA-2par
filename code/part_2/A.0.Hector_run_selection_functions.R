98ecaad220358774ffdd743a2668be549cefaa4d
Dorheim, Kalyn R <kalyn.dorheim@pnnl.gov>
2018-06-29 10:16:12 -0400

    Added functions to select Hector runs based on the 95th and 5th percentiles of a distirbtuion.
    Former-commit-id: 14bc4652b2f2bc245e57884a7ad1a066e465948c [formerly 630596a046357a90763bd977084e0b1812950a97]
    Former-commit-id: 623d2951e7d3a0366dee6377ba1de1989387d426

--- code/part_2/A.0.Hector_run_selection_functions.R
+++ code/part_2/A.0.Hector_run_selection_functions.R
@@ -3,7 +3,7 @@
 # be used in GCAM-Hector.
 
 
-# select_extreeme_values() -------------------------------------------------------------------------------
+# select_extreme_values() -------------------------------------------------------------------------------
 
 # This function selects the extreme max and min values within different filter categories. The function 
 # uses two inputs, a wide data frame containing columns of T/F values indicating whether or not the Hector 
@@ -13,7 +13,7 @@
 # the observational products on top of each other.
 
 
-select_extreeme_values <- function(data, obs_list){
+select_extreme_values <- function(data, obs_list){
   
   # The idea is that the selection function will be applied as many times as there should 
   # be layers of combinations of the observational products. Since we want to do 
@@ -44,25 +44,25 @@ select_extreeme_values <- function(data, obs_list){
         filter_at(filters, all_vars(. == TRUE)) %>% 
         # Add the filter name
         mutate(filter_name = filter_name) %>% 
-        # Subset the passing filter runs for the min and max extreeme values.
+        # Subset the passing filter runs for the min and max extreme values.
         filter(value == min(value) | value == max(value)) %>% 
         select(run_name, variable, year, value, filter_name) -> 
         intermediate
       
-      # Now add a column for indicating if the 2100 value is an extreeme
+      # Now add a column for indicating if the 2100 value is an extreme
       # or a minimum.
       if(nrow(intermediate) == 1){
         
         # If there is only one Hector run that matches the observaitonal 
-        # filter combination then extreeme will = NA.
+        # filter combination then extreme will = NA.
         output <- intermediate
-        output$extreeme <- 'NA'
+        output$extreme <- 'NA'
         
       } else {
         
-        # Add min and max extreem indicators.
+        # Add min and max extreme indicators.
         intermediate %>% 
-          mutate(extreeme = if_else(value == max(value), 'max', 'min')) -> 
+          mutate(extreme = if_else(value == max(value), 'max', 'min')) -> 
           output
       }
       
@@ -72,21 +72,80 @@ select_extreeme_values <- function(data, obs_list){
     }))
     
   }, simplify = F)) -> 
-    filtered_extreems 
+    filtered_extreme
   
   
-  # Now subset the data to find the unfiltered extreemes 
+  # Now subset the data to find the unfiltered extremes 
   data %>% 
     mutate(filter_name = 'None') %>% 
-    # Subset all the Hector runs for the min and max extreeme values.
+    # Subset all the Hector runs for the min and max extreme values.
     filter(value == min(value) | value == max(value)) %>% 
     select(run_name, variable, year, value, filter_name) %>% 
-    mutate(extreeme = if_else(value == max(value), 'max', 'min')) ->
-    unfiltered_extreems
+    mutate(extreme = if_else(value == max(value), 'max', 'min')) ->
+    unfiltered_extreme
   
-  # Concatenate the filtered and unfiltered extreemes into a single data frame 
+  # Concatenate the filtered and unfiltered extreme into a single data frame 
   # and return. 
-  bind_rows(filtered_extreems, unfiltered_extreems)
+  bind_rows(filtered_extreme, unfiltered_extreme)
+  
+}
+
+
+# categorize_runs() -------------------------------------------------------------------------------
+
+# This function categorizes a run into the different observational product filters is matches 
+# based on the Dn metric.
+
+
+categorize_runs <- function(data, obs_list){
+  
+  if( any(! obs_list %in% names(data)) ){stop('Contents of obs_list not in data')}
+  
+  # The idea is that the selection function will be applied as many times as there should 
+  # be layers of combinations of the observational products. Since we want to do 
+  # all of the products individually and then stacked as many times as possible we will 
+  # want to generate the observation combinations for as many times as we have observations.
+  
+  bind_rows(sapply(1:length(obs_list), FUN = function(index){
+    
+
+    # Generate all of the possible combinations for index number 
+    # of stacked observations.
+    filters <- combn(obs_list, index)
+    
+    # Now for each of the filters or possible observational combinations select the 
+    # extreme max and min values for the Hector runs "match" all of the observations 
+    # in the filter.
+    bind_rows(apply(filters, 2, FUN = function(filters){
+      
+      # Format the a filter name based on the observation combination
+      # used to subset the data.
+      filter_name <- paste(filters, collapse = ', ' ) 
+      
+      data %>% 
+        # Subset the data so that all of the Hector variables match
+        # observations according to the Dn / Dc metrics. We can used 
+        # filter_at to do this because we are working with a wide data 
+        # frame that contains T/F if a Hector run has a matching Dn for 
+        # a single variable.
+        filter_at(filters, all_vars(. == TRUE)) %>% 
+        # Add the filter name
+        mutate(filter_name = filter_name)
+      
+    }))
+    
+  }, simplify = F)) -> 
+    categorized_runs
+  
+  
+  # Now categorize the runs into the unfiltered area 
+  data %>% 
+    mutate(filter_name = 'None') ->
+    unfitlered
+  
+  # Concatenate the filtered and unfiltered  runs into a single data frame 
+  # and return. 
+  bind_rows(categorized_runs, unfitlered)
   
 }
 
@@ -126,6 +185,16 @@ bind_rows(sapply(1:length(obs_list), FUN = function(index){
       ungroup -> 
       output
     
+    # If there are no runs that pass through all of the filters then 
+    # add a 0 run count. 
+    if(nrow(output) == 0) {
+      
+      output <- tibble(filter_name = filter_name, count = 0)
+      
+    }
+    
+    output
+    
   }))
   
 }, simplify = F)) -> 
@@ -144,4 +213,45 @@ data %>%
 # and return. 
 bind_rows(filtered_counts, unfiltered_counts)
 
+}
+
+
+# select_95th_values() -----------------------------------------------------------------------
+
+# This function selects runs that most closely resemble the upper and lower 95th CI values 
+# for some input. Requires and data frame that contains a value and a filter_name columns. 
+
+select_95th_values <- function(input){
+  
+  # Check for required columns
+  req_cols <- c('value', 'filter_name')
+  if( any(!req_cols %in% names(input)) ){stop('Input data frame is missing required columns')}
+  
+  # Calculate the mean, sd, and the sample size.
+  mean  <- mean(input[['value']])
+  sd    <- sd(input[['value']] )
+  n     <- length(input[['value']])
+  
+  # Critical value for alpha equal 0.05 * sd / square root of the sample size. 
+  # Assumes a normal distribtuion
+  error <- 1.96 * ( sd / n ^ (.5) )
+  
+  # +/- the error from the mean to get the upper and lower 95 CI values.
+  upper <- mean + error
+  lower <- mean - error
+  
+  # Determine wich index is the closest to the upper and lower CI values. 
+  upper_index <- which.min( abs(input[['value']] - upper) )
+  lower_index <- which.min( abs(input[['value']] - lower) )
+  
+  bind_rows(tibble(run_name = input$run_name[upper_index], 
+                   value = input$value[upper_index],
+                   CI_value = upper, 
+                   CI = 'upper'), 
+            tibble(run_name = input$run_name[lower_index], 
+                   value = input$value[lower_index],
+                   CI_value = lower, 
+                   CI = 'lower')) %>% 
+    mutate(filter_name = unique(input[['filter_name']]))
+  
 }
